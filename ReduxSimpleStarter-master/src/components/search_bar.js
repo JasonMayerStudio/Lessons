8f65af62c1887f90be04dd3cd88501bffa919b28
Jason Mayer <jason@jasonmayer.com>
2018-04-05 12:48:28 -0400

    searchbar styling

--- ReduxSimpleStarter-master/src/components/search_bar.js
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -14,7 +14,7 @@ class SearchBar extends React.Component {
          <input
          value={this.state.term}
          onChange={event => this.setState({ term: event.target.value })} />
-
+        search
       </div>
      );
     }
