92e2fa310663898ab912dbee835afc0dfec7ccc8
Jason Mayer <jason@jasonmayer.com>
2018-04-09 14:02:14 -0400

    shared components

---
+++ ServerSideRendering-React/my-app/shared/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = (props) => {
+
+  render() {
+     return(
+       <div>{homeData}</div>
+
+    );
+
+  }
+
+}
+
+ export default Home;
---
+++ ServerSideRendering-React/my-app/shared/news/news.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const News = () => {
+ render () {
+   return (
+
+   <ol>
+     <li></li>
+     <li></li>
+     <li></li>
+   </ol>
+
+   );
+
+  }
+}
+
+export default News;
