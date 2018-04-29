9eb016c1988131688cc17358ff2502fef52162bc
Jason Mayer <jason@jasonmayer.com>
2018-04-28 23:04:27 -0400

    update list

--- ReduxSimpleStarter-master/src/components/video_list_item.js
+++ ReduxSimpleStarter-master/src/components/video_list_item.js
@@ -3,6 +3,7 @@ import React from 'react';
 const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
 
+
   return (
     <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
